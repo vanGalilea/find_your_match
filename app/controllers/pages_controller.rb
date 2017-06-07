ff7e389b8bc0be01343ea73cbd81398fcbcda052
vanGalilea <z.galili@icloud.com>
2017-06-07 19:30:50 +0200

    updated algorithm, no errors more, check cycle needs to be solved

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -30,19 +30,18 @@ class PagesController < ApplicationController
       @students.each do |student|
         student.matched_user_ids = []
       end
-      puts "*********************************"
+      puts "***************CYCLE!!!!!******************"
     end
   end
 
   def loop_through_students
     check_cycle
-    if @students.first.matched_user_ids.include?(@students.second.id)
-      if @students.length <= 3
-        match_call_back
-        generate_match_of_today
-      end
+    if @students.first.matched_user_ids.include?(@students.second.id) && @students.length > 3
       @students = @students.shuffle
       loop_through_students
+    elsif @students.length <= 3 && @students.first.matched_user_ids.include?(@students.second.id)
+      match_call_back
+      generate_match_of_today
     else
       Match.create(user: @students.first, matched_user:@students.second)
       @students = @students.drop(2)
